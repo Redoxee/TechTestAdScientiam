a453fd4d19b6d0e65d9af2d3987ffa1cd9b8c612
AntonMakesGames <antonmakesgames@gmail.com>
2023-09-06 17:40:56 +0200

    Added EntityAlreadyExistException

---
+++ src/TestTechnique.Application/Exceptions/EntityAlreadyExistException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestTechnique.Application.Exceptions;
+
+internal class EntityAlreadyExistException : Exception
+{
+    public EntityAlreadyExistException()
+    {
+    }
+
+    public EntityAlreadyExistException(string message) : base(message)
+    {
+    }
+
+    public EntityAlreadyExistException(string message, Exception innerException) : base(message, innerException) 
+    {
+    }
+
+    public EntityAlreadyExistException(SerializationInfo serializationInfo, StreamingContext streamingContext) :
+        base(serializationInfo, streamingContext)
+    {
+    }
+}
