d9196ab477974776761149f400796d22e7496792
AntonMakesGames <antonmakesgames@gmail.com>
2023-09-06 17:38:14 +0200

    Added method to convert ProductDto to Domain Product

--- src/TestTechnique.Application/Extensions/ProductDtoExtension.cs
+++ src/TestTechnique.Application/Extensions/ProductDtoExtension.cs
@@ -13,9 +13,25 @@ namespace TestTechnique.Application.Extensions
                 Id = product.Id,
                 Name = product.Name,
                 Description = product.Description,
+                Price = product.Price,
                 // TODO : is the right way to convert brand to string or should it be handled by ToString ?
                 Brand = product.Brand.Name,
+            };
+        }
+
+        public static Product To(this ProductDto product) {
+            return new Product
+            {
+                Id = product.Id,
+                Name = product.Name,
+                Description = product.Description,
                 Price = product.Price,
+                // TODO : there's definitely something missing in this conversion concerning the Brand
+                Brand = new Brand
+                {
+                    Id = Guid.Empty,
+                    Name = product.Brand,
+                }
             };
         }
     }
