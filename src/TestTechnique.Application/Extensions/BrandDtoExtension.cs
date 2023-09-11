e1b000cbc813ffd751d8c68b3a6c0fbb239f313d
AntonMakesGames <antonmakesgames@gmail.com>
2023-09-11 20:12:04 +0200

    Added extension methods for brandDto To and From

--- src/TestTechnique.Application/Extensions/BrandDtoExtension.cs
+++ src/TestTechnique.Application/Extensions/BrandDtoExtension.cs
@@ -2,6 +2,16 @@
 using TestTechnique.Domain.Models;
 
 namespace TestTechnique.Application.Extensions;
+
 internal static class BrandDtoExtension
 {
+	public static BrandDto From(this Brand brand)
+	{
+		return new BrandDto { Id = brand.Id, Name = brand.Name };
+	}
+
+	public static Brand To(this BrandDto brand)
+	{
+		return new Brand { Id = brand.Id, Name = brand.Name};
+	}
 }
