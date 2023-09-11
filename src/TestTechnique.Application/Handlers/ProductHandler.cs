6d4ec2b404e5dd1f840e67b5ba4e44c915a6687c
AntonMakesGames <antonmakesgames@gmail.com>
2023-09-11 19:47:09 +0200

    Fixes on product handler

--- src/TestTechnique.Application/Handlers/ProductHandler.cs
+++ src/TestTechnique.Application/Handlers/ProductHandler.cs
@@ -3,6 +3,7 @@ using TestTechnique.Application.Contracts;
 using TestTechnique.Application.Repositories;
 using TestTechnique.Application.Extensions;
 using TestTechnique.Application.Exceptions;
+using TestTechnique.Domain.Models;
 
 namespace TestTechnique.Application.Handlers;
 
@@ -10,12 +11,14 @@ public class ProductHandler : IProductHandler
 {
     private readonly IUnitOfWork _unitOfWork;
     private readonly IProductRepository _productRepository;
+	private readonly IBrandRepository _brandRepository;
 
-    public ProductHandler(IUnitOfWork unitOfWork, IProductRepository productRepository)
+	public ProductHandler(IUnitOfWork unitOfWork, IProductRepository productRepository, IBrandRepository brandRepository)
     {
         _unitOfWork = unitOfWork ?? throw new ArgumentNullException(nameof(unitOfWork));
         _productRepository = productRepository ?? throw new ArgumentNullException(nameof(productRepository));
-    }
+		_brandRepository = brandRepository ?? throw new ArgumentNullException(nameof(brandRepository));
+	}
 
     public async Task<IEnumerable<ProductDto>> GetAllAsync()
     {
@@ -26,33 +29,59 @@ public class ProductHandler : IProductHandler
     public async Task<ProductDto> GetAsync(Guid id)
     {
         var product = await _productRepository.GetAsync(id);
+       if (product == null)
+        {
+            throw new EntityNotFoundException();
+        }
 
         return product.From();
     }
 
     public async Task<Guid> AddAsync(ProductDto productDto)
     {
-        var alreadyPresentProduct = await _productRepository.GetAsync(productDto.Id);
+        if (productDto == null)
+        {
+            throw new ArgumentNullException($"Argument: {nameof(productDto)} is null");
+		}
+
+		if (productDto.Brand == null)
+		{
+			throw new ArgumentException($"Argument: {nameof(productDto.Brand)} is null");
+		}
+
+		var alreadyPresentProduct = await _productRepository.GetAsync(productDto.Id);
         if(alreadyPresentProduct != null)
         {
             throw new EntityAlreadyExistException($"Product already exist Id:{productDto.Id}");
         }
 
-        var result = await _productRepository.AddAsync(productDto.To());
+        Brand brand = await _brandRepository.GetBrandByName(productDto.Brand);
+        var result = await _productRepository.AddAsync(productDto.To(brand));
         await _unitOfWork.SaveChangesAsync();
 
         return result;
     }
 
     public async Task<ProductDto> UpdateAsync(ProductDto productDto)
-    {
-        var product = await _productRepository.GetAsync(productDto.Id);
+	{
+		if (productDto == null)
+		{
+			throw new ArgumentNullException($"Argument: {nameof(productDto)} is null");
+		}
+
+        if (productDto.Brand == null)
+        {
+            throw new ArgumentException($"Argument: {nameof(productDto.Brand)} is null");
+        }
+
+		var product = await _productRepository.GetAsync(productDto.Id);
         if (product == null)
         {
             throw new EntityNotFoundException($"Product not found Id:{productDto.Id}");
         }
 
-        product = productDto.To();
+		Brand brand = await _brandRepository.GetBrandByName(productDto.Brand);
+		product = productDto.To(brand);
         await _productRepository.UpdateAsync(product);
         await _unitOfWork.SaveChangesAsync();
         return product.From();
